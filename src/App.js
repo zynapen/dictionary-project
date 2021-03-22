9dcc7a5ba1969a01cc689ffc7b4301cc53b7c329
Zyna Pen <zynapen@gmail.com>
2021-03-22 09:52:41 -0400

    added dictionary app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import logo from "./logo.png";
-import './App.css';
+import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <img src={logo} className="App-logo img-fluid" alt="logo" />
       </header>
+      <main>
+        <Dictionary />
+      </main>
       <footer className="App-footer">
         This project was coded by <a href="https://www.linkedin.com/in/zynapen/" target="_blank" rel="noreferrer">Zyna Pen</a>
         <br />
         Open-sourced on <a href="https://github.com/zynapen/dictionary-project" target="_blank" rel="noreferrer">GitHub</a> and hosted on <a href="https://vigilant-mclean-242a81.netlify.app/" target="_blank" rel="noreferrer">Netifly</a>
       </footer>
+      </div>
     </div>
   );
 }
