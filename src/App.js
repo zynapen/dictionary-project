b535dde2666f270a9f34f72bceee824e5ada4e1e
Zyna Pen <zynapen@gmail.com>
2021-03-22 08:54:01 -0400

    added footer

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ export default function App() {
       <header className="App-header">
         <img src={logo} className="App-logo img-fluid" alt="logo" />
       </header>
+      <footer className="App-footer">
+        This project was coded by <a href="https://www.linkedin.com/in/zynapen/" target="_blank" rel="noreferrer">Zyna Pen</a>
+        <br />
+        Open-sourced on <a href="https://github.com/zynapen/dictionary-project" target="_blank" rel="noreferrer">GitHub</a> and hosted on <a href="https://vigilant-mclean-242a81.netlify.app/" target="_blank" rel="noreferrer">Netifly</a>
+      </footer>
     </div>
   );
 }
