5f2d1c0bebbfff911c5c9ab1c3c23e164c3ec412
Zyna Pen <zynapen@gmail.com>
2021-03-22 10:01:12 -0400

    added dictionary app

---
+++ src/Dictionary.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+    let [keyword, setKeyword] = useState("");
+
+    function search (event) {
+        event.preventDefault();
+        alert(`Searching for ${keyword} definition`);
+    }
+
+    function handleKeywordChange(event) {
+        //console.log(event.target.value);
+        setKeyword(event.target.value);
+    }
+
+    return (
+        <div className="Dictionary">
+            <form onSubmit={search}>
+                <input type="search" onChange={handleKeywordChange} />
+            </form>
+        </div>
+        )
+}
