26d637b10997e7b500f3790faa3a88676250129a
Zyna Pen <zynapen@gmail.com>
2021-03-24 13:34:43 -0400

    fixed link

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -4,7 +4,7 @@ export default function Phonetic(props) {
     //console.log(props.phonetic);
     return (
         <div className="Phonetic">
-            <a href={props.phonetic.audio} target="_blank">
+            <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
                 Listen
             </a>  {""}
             {props.phonetic.text}
