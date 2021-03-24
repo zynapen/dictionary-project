544eb3ff6fc0157a5cd09d8931eba79e026c1023
Zyna Pen <zynapen@gmail.com>
2021-03-24 13:18:01 -0400

    Rendered Phonetic

---
+++ src/Phonetic.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Phonetic(props) {
+    //console.log(props.phonetic);
+    return (
+        <div className="Phonetic">
+            <a href={props.phonetic.audio} target="_blank">
+                Listen
+            </a>  {""}
+            {props.phonetic.text}
+        </div>
+    )
+}
--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function Synonyms(props) {
-    console.log(props.synonyms);
+    //console.log(props.synonyms);
     if(props.synonyms) {
         return (
         <ul className="Synonyms">
