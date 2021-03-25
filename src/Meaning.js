8d4b7c92a3868e52eb8bbc3c82e35bd38684e9b2
Zyna Pen <zynapen@gmail.com>
2021-03-25 15:03:40 -0400

    Created div for definition and example

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Synonyms from "./Synonyms";
+import "./Meaning.css";
 
 export default function Meaning(props) {
     //console.log(props.meaning);
@@ -10,10 +11,8 @@ export default function Meaning(props) {
                 return (
                     <div key={index}>
                         <p>
-                        <strong>Definition:</strong> {definition.definition}
-                        <br />
-                        <strong>Example:</strong> <em>{definition.example}</em>
-                        <br />
+                        <div className="definition"> {definition.definition}</div>
+                        <div className="example">{definition.example}</div>
                         <Synonyms synonyms={definition.synonyms} />
                         </p>
                         </div>
