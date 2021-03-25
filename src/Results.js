4e4a0e2aed876588d2c7bdd16bc30f6027c88e35
Zyna Pen <zynapen@gmail.com>
2021-03-25 14:27:18 -0400

    Created Section

--- src/Results.js
+++ src/Results.js
@@ -8,6 +8,7 @@ export default function Results(props) {
     if (props.results) {
         return (
         <div className="Results">
+            <section>
             <h2>{props.results.word}</h2>
             {props.results.phonetics.map(function(phonetic, index) {
                 return (
@@ -16,11 +17,13 @@ export default function Results(props) {
                     </div>
                 );
             })}
+            </section>
             {props.results.meanings.map(function(meaning, index) {
                 return (
-                    <div key={index}>
+                    <section key={index}>
                         <Meaning meaning={meaning} />
-            </div>
+            </section>
+            
         );
     })}
         </div>
