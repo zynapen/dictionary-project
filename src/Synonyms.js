a23c12dfbf680ea1a996fd30b7cc6b774cf57238
Zyna Pen <zynapen@gmail.com>
2021-03-26 19:50:42 -0400

    Updated Synonyms

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -5,7 +5,7 @@ export default function Synonyms(props) {
     //console.log(props.synonyms);
     if(props.synonyms) {
         return (
-        <ul className="Synonyms">
+        <ul className="Synonyms"><h6>Synonyms:</h6>
         {props.synonyms.map(function (synonym, index) {
             return <li key={index}>{synonym}</li>;
         })}
