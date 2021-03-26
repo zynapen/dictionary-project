c30f66759b8231a44b01e8a5904dff2b4e6b577b
Zyna Pen <zynapen@gmail.com>
2021-03-26 19:58:59 -0400

    Updated alt attribute

--- src/Photos.js
+++ src/Photos.js
@@ -9,7 +9,7 @@ export default function Photos(props) {
             {props.photos.map(function (photo, index) { 
                 return (<div className="col-4" key={index}>
                     <a href={photo.src.original} target="_blank" rel="noreferrer">
-                    <img src={photo.src.tiny} className="img-fluid" alt=" " /></a>
+                    <img src={photo.src.tiny} className="img-fluid" alt={photo.src.photographer} /></a>
                     </div>
                     );
         })}
