84abda7e108505cb405be874ca1a83eca0eb1263
Zyna Pen <zynapen@gmail.com>
2021-03-26 18:38:19 -0400

    Created Photos API from Pexels

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,15 +1,22 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
     let [results, setResults] = useState(null);
+    let [photos, setPhotos] = useState(null);
 
-    function handleResponse(response) {
+    function handleDictionaryResponse(response) {
         //console.log(response.data[0].meanings[0].definitions[0].definition);
         setResults(response.data[0]);
+    }
+
+    function handlePexelsResponse(response) {
+        //console.log(response);
+        setPhotos(response.data[0]);
     }
 
     function search (event) {
@@ -17,8 +24,13 @@ export default function Dictionary() {
 
         //documentation: https://dictionaryapi.dev/
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-        axios.get(apiUrl).then(handleResponse);
+        axios.get(apiUrl).then(handleDictionaryResponse);
     }
+        //documentation: https://www.pexels.com/api/documentation/
+        let pexelsApiKey = "563492ad6f91700001000001439b950cda554010b63a13c1ea4c01d5";
+        let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+        let headers = { Authorization : `Bearer ${pexelsApiKey}` };
+        axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
 
     function handleKeywordChange(event) {
         setKeyword(event.target.value);
@@ -33,7 +45,7 @@ export default function Dictionary() {
             <div className="hint">Suggested words: Advocate, Aesthetic, Agitate, Ambitious...</div>
             </section>
             <Results results={results}/>
-            
+            <Photos photos={photos} />
         </div>
         )
 }
