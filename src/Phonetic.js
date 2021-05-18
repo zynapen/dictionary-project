c09817017178cd748c1e6e860662aed4a00d685b
Zyna Pen <zynapen@gmail.com>
2021-05-18 09:40:17 -0400

    Corrected Prop with Boolean Type

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -4,13 +4,13 @@ import "./Phonetic.css"
 
 export default function Phonetic(props) {
     //console.log(props.phonetic);
+    //https://www.npmjs.com/package/react-audio-player;
     return (
         <div className="Phonetic">
             {props.phonetic.text}
             <br />
             <ReactAudioPlayer
             src={props.phonetic.audio}
-            onPlay
             controls
             />
         </div>
