472613b992851d9938d1f3550c416e2965e4f337
Zyna Pen <zynapen@gmail.com>
2021-05-18 09:42:29 -0400

    Fixed API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,49 +4,68 @@ import Results from "./Results";
 import Photos from "./Photos";
 import "./Dictionary.css";
 
-export default function Dictionary() {
-    let [keyword, setKeyword] = useState("");
-    let [results, setResults] = useState(null);
-    let [photos, setPhotos] = useState(null);
+export default function Dictionary(props) {
+  let [keyword, setKeyword] = useState(props.defaultKeyword);
+  let [results, setResults] = useState(null);
+  let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-    function handleDictionaryResponse(response) {
-        //console.log(response.data[0].meanings[0].definitions[0].definition);
-        setResults(response.data[0]);
-    }
+  function handleDictionResponse(response) {
+    setResults(response.data[0]);
+  }
 
-    function handlePexelsResponse(response) {
-        //console.log(response);
-        setPhotos(response.data.photos);
-    }
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
 
-    function search (event) {
-        event.preventDefault();
+  function search() {
+    // documentation: https://dictionaryapi.dev/e
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleDictionResponse);
 
-        //documentation: https://dictionaryapi.dev/
-        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-        axios.get(apiUrl).then(handleDictionaryResponse);
-    }
-        //documentation: https://www.pexels.com/api/documentation/
-        let pexelsApiKey = "563492ad6f91700001000001439b950cda554010b63a13c1ea4c01d5";
-        let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
-        let headers = { Authorization : `Bearer ${pexelsApiKey}` };
-        axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
-        
+    let pexelsApiKey =
+      "563492ad6f91700001000001439b950cda554010b63a13c1ea4c01d5";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
+  }
 
-    function handleKeywordChange(event) {
-        setKeyword(event.target.value);
-    }
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
 
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  function load() {
+    setLoaded(true);
+    search();
+  }
+
+  if (loaded) {
     return (
-        <div className="Dictionary">
-            <section>
-            <form onSubmit={search}>
-                <input type="search" placeholder="Search" onChange={handleKeywordChange} />
-            </form>
-            <div className="hint">Suggested words: Advocate, Aesthetic, Agitate, Ambitious...</div>
-            </section>
-            <Results results={results}/>
-            <Photos photos={photos} />
-        </div>
-        )
+      <div className="Dictionary">
+        <section>
+          <h1>What's the word?</h1>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="search"
+              onChange={handleKeywordChange}
+              defaultValue={props.defaultKeyword}
+            />
+          </form>
+          <div className="hint">
+            suggested words: creator, designer, organizer, planner...
+          </div>
+        </section>
+        <Results results={results} />
+        <Photos photos={photos} />
+      </div>
+    );
+  } else {
+    load();
+    return "Loading";
+  }
 }
