4447277e9d7d430099d32bdbf92ec415c48441d1
Zyna Pen <zynapen@gmail.com>
2021-05-24 15:27:13 -0400

    Update Footer

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ export default function App() {
       <Dictionary defaultKeyword="developer" />
       </main>
       <footer className="App-footer">
-        This project was coded by <a href="https://www.linkedin.com/in/zynapen/" target="_blank" rel="noreferrer">Zyna Pen</a>
+        This project was coded by <a href="https://goofy-dubinsky-84ab58.netlify.app/" target="_blank" rel="noreferrer">Zyna Pen</a>
         <br />
         Open-sourced on <a href="https://github.com/zynapen/dictionary-project" target="_blank" rel="noreferrer">GitHub</a> and hosted on <a href="https://vigilant-mclean-242a81.netlify.app/" target="_blank" rel="noreferrer">Netifly</a>
       </footer>
